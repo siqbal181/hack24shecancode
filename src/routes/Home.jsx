5665c2ead2d5769c7a3a3855f83b9696c0db85ab
Rossana Ventrella <rossana.ventrella@checkatrade.com>
2024-03-28 16:30:56 +0000

    small change

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,27 +6,6 @@ import banner from '../assets/banner.jpg'
 
 
 const Home = () => {
-  // const [data, setData] = useState("initial data");
-
-  // useEffect(() => {
-  //   console.log("useEffect");
-  //   async function fetchData() {
-  //     const response = await fetch("data.json");
-  //     const data = await response.json();
-  //     setData(data.data[0].job_name);
-  //     return data;
-  //   }
-  //   fetchData();
-  // }, []);
-
-  // async function handleSubmit() {
-  //   console.log("submit");
-  //   const response = await fetch("data.json");
-  //   const data = await response.json();
-  //   console.log("submit");
-  //   setData(data.data[0].tag_categories[0]);
-  //   return data;
-  // }
 
   return (
     <>
